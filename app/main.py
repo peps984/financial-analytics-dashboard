e7148d77063d06ac4db6a1985ea989ce87f692e3
peps984 <peppe984@gmail.com>
2025-12-21 18:07:07 +0100

    add config and .env files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.APP_NAME)
 
 @app.get("/")
 def read_root():
-    return{"message": "Hello World"}
+    return{
+        "message": "Hello World",
+        "app_name": settings.APP_NAME
+    }
+
+@app.get("/config")
+def show_config():
+    return{
+        "app_name": settings.APP_NAME,
+        "database_url": settings.DATABASE_URL[:20] + "..."
+    }
