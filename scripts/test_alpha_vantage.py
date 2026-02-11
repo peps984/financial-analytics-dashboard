0ba71ac2e9c5499553736a41d52c77d8f90a5000
peps984 <peppe984@gmail.com>
2026-02-11 18:04:54 +0100

    fix error importing from parant sub

--- scripts/test_alpha_vantage.py
+++ scripts/test_alpha_vantage.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# Adjustment needed to import modules from parent subdirectories
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from app.services.alpha_vantage import AlphaVantageClient
 
 client = AlphaVantageClient()
